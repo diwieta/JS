efe3d3cf7ebb9d07d87dec7ab25fea9b1f863b05
diwieta <110148670+diwieta@users.noreply.github.com>
2025-04-21 11:52:17 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //this will treat JS code as newer version //engine will treat the whole code according to new standards
+
+//alert (3 + 3)// this can only be used in browser and not in node.js
+
+//read ability is very imp in coding//
+
+let name = "diwi"
+let age = 18
+let isLoggedIn = false
+let state; //this is undefined not null
+
+/*
+primitive datatype
+
+number => 2 power 53
+bigint
+string used with "" or ''
+boolean => true/false
+null => it is stand alone value, representation of empty value. it can be assigned too.
+undefined => when value is not defined, but variable declared. abhi value assign nai hui hai
+symbol => for unique
+
+
+object
+*/
+
+console.log(typeof "diwi");
+console.log(typeof age);
+console.log(typeof null); //result is "object"
+console.log(typeof undefined); //result is undefined
+
+
